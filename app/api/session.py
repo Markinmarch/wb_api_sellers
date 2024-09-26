de5551b2711a869348801341d0b4d2e4822ea7ef
Markinmarch <iztvmrk@mail.ru>
2024-09-26 21:59:48 +0300

    refactory

--- app/api/session.py
+++ app/api/session.py
@@ -1,7 +1,7 @@
 import aiohttp, asyncio
 
-# from core.config import WB_API_TOKEN, URL_OPENAPI, URL_DIGITAL, PATH
 from ..core.config import WB_API_TOKEN, URL_OPENAPI, URL_DIGITAL, PATH
+
 
 class Session:
     
@@ -18,16 +18,7 @@ class Session:
             async with session.get(
                 url = self.url + self.path,
                 headers = self.headers,
-                params = {'Authorization': self.auth},
-                json = {
-                    'Authorization': self.auth
-                }
             ) as respond:
-                print(self.url+self.path)
-                return respond
-            
-
-            
-            
+                return respond.status
+                        
 login = Session().login()
-print(WB_API_TOKEN)
